d3d72f9afca83f2acb2d12d31310a4ca32090f59
Myronic <myron.c@somaiya.edu>
2018-11-04 17:00:11 +0530

    added sticky footbar

--- post.php
+++ post.php
@@ -10,12 +10,29 @@
      <!-- for inline icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
     <link rel="stylesheet" href="post.css">
+
+
+    <style>
+        #cap{
+            white-space: nowrap; 
+            width: 45vw; 
+            overflow: hidden;
+            text-overflow: ellipsis; 
+            word-wrap: break-word;
+        }
+
+     #cap:hover {
+         overflow: visible;
+         white-space: wrap; 
+         word-wrap: break-word;
+     }
+    </style>
 </head>
 <body>
     <div class="card postCard">
         <div class="card-header">
             <div class="row">
-                <div class="col-sm-1"><img src="Logo Final.png" class="rounded-circle postImg" alt="Profile"></div>
+                <div class="col-sm-1"><img src="shuttergram.png" class="rounded-circle postImg" alt="Profile"></div>
                 <div class="col-sm-11 h4 align-self-center postTitle"><?php echo $_SESSION['username']; ?></div>
             </div>
         </div>
@@ -25,15 +42,11 @@
         <div class="card-footer">
             <h3>
                 <i class='far'>&#xf004;</i>&nbsp;
-                <span class="h5">665</span>&nbsp;
-                <i  style=";" class='far farCustom'>&#xf27a;</i>
+                <span class="h5 my-auto">665</span>&nbsp;
+                <i class='far farCustom'>&#xf27a;</i>
             </h3>
             <a class="h5 username" href="#"><?php echo $_SESSION['username']; ?></a>
-            <span class="muted"><?php echo $_SESSION['caption']; ?></small>
-            <button class="btn btn-outline-dark btn-sm more" data-target="#caption<?php echo $_SESSION['index']; ?>" class="muted" data-toggle="collapse">more</button>
-            <div id="caption<?php echo $_SESSION['index']; ?>" class="collapse">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptate iure dolor, iusto, recusandae non quae, blanditiis incidunt numquam odit voluptatibus minima possimus atque impedit officia nostrum dolore ut ducimus!
-            </div>
+            <div id="cap"><span class="muted"><?php echo $_SESSION['caption']; ?></span></div>
         </div>
     </div>
 </body>
